cab0013e99429b9d0d0d5c4dd7b35fd33361bf46
tang <2223254825@qq.com>
2019-11-26 11:02:13 +0800

    create dirs

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view></router-view>
+    <FooterGuide></FooterGuide>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FooterGuide from './components/FooterGuide/FooterGuide'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    FooterGuide
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+#app
+  width: 100%
+  height 100%
+  background-color #f5f5f5
 </style>
---
+++ src/components/FooterGuide/FooterGuide.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        FoodGuide
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "FooterGuide"
+    }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+
+</style>

403b3bbb277cec12a9894fd1937feb6ec1960ca3
tang <2223254825@qq.com>
2019-11-26 19:05:16 +0800

    feature msite

--- src/api/ajax.js
+++ src/api/ajax.js
@@ -1,39 +1,42 @@
 import Axios from 'axios';
 
-// export default function ajax(url='', data={}, type='GET') {
-//     return new Promise(function (resolve, reject) {
-//         let promise;
-//         if(type === 'GET'){
-//             let dataStr = '';
-//             Object.keys(data).forEach(key =>{
-//                 dataStr += key + "=" + data[key] + "&"
-//             })
-//             if(dataStr !== ''){
-//                 dataStr = dataStr.substring(0, dataStr.lastIndexOf('&'));
-//                 url = url + "?" + dataStr
-//             }
-//         promise = Axios.get(url)
-//         }else{
-//             promise = Axios.post(url, data);
-//         }
-//
-//         promise.then(response =>{
-//             resolve(response.data)
-//         })
-//             .catch(error=>{
-//                 reject(error)
-//             })
-//     })
-//
-// }
-export default async function ajax(url='', data={}, type='GET') {
-    return await Axios({
-        method: type,
-        params: data,
-        url:url
-    }).then(response=>{
-        return response.data
-    }, error=>{
-        return error
+export default function ajax(url='', data={}, type='GET') {
+    return new Promise(function (resolve, reject) {
+        let promise;
+        if(type === 'GET'){
+            let dataStr = '';
+            Object.keys(data).forEach(key =>{
+                dataStr += key + "=" + data[key] + "&"
+            })
+            if(dataStr !== ''){
+                dataStr = dataStr.substring(0, dataStr.lastIndexOf('&'));
+                url = url + "?" + dataStr
+            }
+        promise = Axios.get(url)
+        }else{
+            promise = Axios.post(url, data);
+        }
+
+        promise.then(response =>{
+            resolve(response.data)
+        })
+            .catch(error=>{
+                reject(error)
+            })
     })
+
 }
+
+
+
+// export default async function ajax(url='', data={}, type='GET') {
+//     return await Axios({
+//         method: type,
+//         params: data,
+//         url:url
+//     }).then(response=>{
+//         return response.data
+//     }, error=>{
+//         return error
+//     })
+// }
